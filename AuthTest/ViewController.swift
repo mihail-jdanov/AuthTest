be9baa74af25f7479b489a110cbe21de0647a027
Mikhail Zhdanov <mihail.jdanov1993@gmail.com>
2022-09-22 20:48:15 +0700

    Created project

---
+++ AuthTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AuthTest
+//
+//  Created by Михаил Жданов on 22.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
